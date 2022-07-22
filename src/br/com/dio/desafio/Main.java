81150867f32c0002e7b952b0076008097816846e
natashasilveira <natashasilveira.p@gmail.com>
2022-07-22 18:28:39 -0300

    Criação das classes Mentoria, Curso e Main

---
+++ src/br/com/dio/desafio/Main.java
@@ -0,0 +1,29 @@
+package br.com.dio.desafio;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setTitulo("curso java");
+        curso.setDescricao("descrição curso java");
+        curso.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso javascript");
+        curso2.setDescricao("descrição curso javascript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
